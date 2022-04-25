96b5d9fa36175e26241d3a376af0e57ae36950a2
Deepak Meher <deepakmeher777@gmail.com>
2022-04-26 00:05:38 +0530

    Changes to conf.ts

--- conf.ts
+++ conf.ts
@@ -1,5 +1,17 @@
 exports.config = {
     framework: 'jasmine',
-    seleniumAddress: 'http://localhost:4444/wd/hub',
-    specs: ['spec.js']
+    directConnect:true,
+    specs: ['spec.ts'],
+    capabilities: { 
+      'browserName': 'chrome', 
+    },
+    useAllAngular2AppRoots: true,
+    onPrepare() { 
+      require('ts-node').register({ 
+        project: require('path').join(__dirname, './tsconfig.json') // Relative path of tsconfig.json file 
+      });
+    },
+    onComplete() { 
+      //global test tear-down goes here 
+    }
   }
