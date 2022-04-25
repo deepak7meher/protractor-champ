961f23e965fcac64e6cfa424268adfbf4dbddd8d
Deepak Meher <deepakmeher777@gmail.com>
2022-04-26 00:19:30 +0530

    Changes to conf.ts

---
+++ src/home-spec.ts
@@ -0,0 +1,10 @@
+import { browser } from "protractor";
+
+describe('Protractor Demo App', function() {
+    it('should have a title', function() {
+        console.info("New test");
+      browser.get('http://juliemr.github.io/protractor-demo/');
+  
+      expect(browser.getTitle()).toEqual(new Promise(()=>{}));
+    });
+  });
